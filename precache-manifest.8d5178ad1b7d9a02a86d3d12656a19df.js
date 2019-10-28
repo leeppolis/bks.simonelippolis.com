78208332e2efd05a1ab1766c58ca3a04a68f2025
Gitlab auto-deploy <213991+leeppolis@users.noreply.github.com>
2019-10-28 14:06:22 +0000

    Ready to deploy

---
+++ precache-manifest.8d5178ad1b7d9a02a86d3d12656a19df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86b1df05f10109c2158a",
+    "url": "/static/css/main.dd398e80.chunk.css"
+  },
+  {
+    "revision": "86b1df05f10109c2158a",
+    "url": "/static/js/main.86b1df05.chunk.js"
+  },
+  {
+    "revision": "161744abee29b7cc3ebf",
+    "url": "/static/js/1.161744ab.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "68945ae5083747ea036526c231d886b1",
+    "url": "/index.html"
+  }
+];
